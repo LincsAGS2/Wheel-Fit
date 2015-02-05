ffb4e7e4d73710fd12b96d4018022e4549a394c1
Ashley Blake-Hood <ashbh@hotmail.co.uk>
2015-02-05 11:45:35 +0000

    Added Wheelchair Plugin and docs

---
+++ Unity Plugins/unity/Package_02_03_2015/Package 02_03_2015/TankGameInputUnityTest/Assets/Scripts/SoldierController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using TankGameInput;
+
+public class SoldierController : MonoBehaviour
+{
+
+	public InputController inputController;
+
+	// Use this for initialization
+	void Start ()
+	{
+		return;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Get the input info
+		GameInputInfo inputInfo = this.inputController.InputInfo;
+
+		// Set the soldier position
+		if(inputInfo.SoldierInfo.IsSkeletonAvailable)
+			this.transform.position = Vector3.right * 3f * inputInfo.SoldierInfo.Position;
+		else
+			this.transform.position = Vector3.zero;
+
+		if(inputInfo.SoldierInfo.IsSkeletonAvailable)
+			Debug.Log("Skeleton available.");
+
+		return;
+	}
+}
