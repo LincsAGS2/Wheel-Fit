ff8e416eb7c85f60d92df2cb38aee158b21f2a7e
DeathLord00 <a.deathridge@btinternet.com>
2015-02-25 12:22:59 +0000

    All Work Andrew has done
    Chunk code and hour timer

---
+++ WheelFit/Assets/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	//float startTime;
+	float currentTime;
+	[Range(0.0f,3600.0f)]
+	public float timer = 3600;
+	//public float timerIncrement;
+
+	// Use this for initialization
+	void Start () 
+	{
+		currentTime = 0;
+		//startTime = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentTime += Time.unscaledDeltaTime;
+		print (currentTime);
+		
+		if (currentTime >= timer) 
+		{
+			currentTime = 0;
+			print ("You have been working hard. Have a break. HAVE A KIT-KAT!");
+		}
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+}
---
+++ WheelFit/Assets/WorldGen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WorldGen : MonoBehaviour{
+
+	Chunk chunk;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//chunk = new Chunk();
+		chunk = gameObject.GetComponent<Chunk>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
