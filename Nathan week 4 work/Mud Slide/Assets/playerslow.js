d9ae33584a0650b17aeb1681d1a0424c8044d829
a <a>
2015-03-05 12:56:22 +0000

    Nathan work for week 4
    contains ski level models the skiis, helmet and goggles. Also contains
    the mud slide work

---
+++ Nathan week 4 work/Mud Slide/Assets/playerslow.js
@@ -0,0 +1,28 @@
+﻿	// collision event script attached to a ParticleSystem
+	// applies a force to rigid bodies that are hit by particles
+	private var collisionEvents = new ParticleSystem.CollisionEvent[16];
+	function OnParticleCollision(other : GameObject)
+ {
+ 
+		// adjust array length
+		var safeLength = particleSystem.safeCollisionEventSize;
+		if (collisionEvents.Length < safeLength) 
+		{
+			collisionEvents = new ParticleSystem.CollisionEvent[safeLength];
+		}
+		
+		// get collision events for the gameObject that the script is attached to
+		var numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
+		
+		// apply some force to RigidBody components
+		for (var i = 0; i < numCollisionEvents; i++)
+		   {
+			if (other.rigidbody)
+		   {
+	 		var pos = collisionEvents[i].intersection;
+			var force = collisionEvents[i].velocity * 10;
+			other.rigidbody.AddForce(Vector3.back * 3.5);
+			}
+		}
+	}
+	
