21f976c57503ae7fcec276c106f0c172da2c7811
Thomas-Fisher <thomas-fisher@live.co.uk>
2015-03-05 06:50:00 +0000

    Water animation and effects created
    Created water using the unity free version as a base, I edited the free
    shaders they provide to change the alpha as well as add transparency. I
    also created a wave generation script to make the water appear
    realistic. Created a very basic buoyancy script which causes the boat to
    appear to bob up and down to provide realism
    
    New scene created called WaterGame
    Boat prefab created to test buoyancy script
    Basic particle emitters around the boat to give the effect of
    interacting with the water
    
    Prefabs created for everything in the scene, Cameras, boat, light and
    water

---
+++ Wheel Fit/Assets/Scripts/Boat Game/BasicBuoyancy.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicBuoyancy: MonoBehaviour
+{
+	//Buoyancy Variables
+	public GameObject Water;
+	public float DownForce = 2;
+	public float UpForce = 1;
+
+	//Fake Buoyancy Variables
+	public float CurrentYPosition;
+	public bool Loop;
+
+	// Use this for initialization
+	void Start () 
+	{
+		CurrentYPosition = transform.position.y;
+		Loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Buoyancy();//This Worked, but was to bouncy, may be usefull in the 
+	FakeBuoyancy();
+	}
+
+	void FakeBuoyancy()
+	{
+		if (Loop == true) // if loop is true then the object it going up
+		{			
+			if (CurrentYPosition >= 0.25f)
+			{
+				Debug.Log ("entered");
+				Loop = false;
+			} 
+			else 
+			{
+				CurrentYPosition += 0.005f; // adds 0.005f to the Y axis
+			}
+		} 
+		else // if loop is falue then the object is going down
+			if (CurrentYPosition <= -0.25f)
+		{
+			Loop = true;
+		}
+		else 
+		{
+			CurrentYPosition -= 0.005f;//removeds 0.005f to the Y axis
+		}
+		Vector3 newpos = new Vector3(transform.position.x,CurrentYPosition,transform.position.z); // creates a holder vec3
+		transform.position = newpos; // updates the objects position to the new y position
+	}
+
+	void Buoyancy()
+	{
+		 
+		//pushes the object up towards the water
+		if(transform.position.y > Water.transform.position.y)
+		{
+		//dont really need this ad we are not dropping it to hard into the water
+		//	rigidbody.AddForce(-Vector3.up * DownForce);
+		}
+
+		//pushes the object down into the water ( incase it bounced to far up )
+		if(transform.position.y < Water.transform.position.y+1)
+		{
+			rigidbody.AddForce(Vector3.up * UpForce);				
+		}
+	}
+}
---
+++ Wheel Fit/Assets/Scripts/Boat Game/WaveGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveGenerator : MonoBehaviour
+{
+	public float Scale = 0.25f;
+	public float WaveSpeed = 1.0f;
+	public float NoiseStrength = 1f;
+	public float NoiseDistance = 1f;
+	
+	private Vector3[] BaseHeight;
+	
+	void Update () {
+
+		Mesh WaterMesh = GetComponent<MeshFilter>().mesh; // watermech contains the mesh data of the gameobject this script is attached to
+		
+		if (BaseHeight == null)// checks if BaseHeight is null
+		{
+			BaseHeight = WaterMesh.vertices; // Adds the meshes current vertices to the BaseHeight array
+		}
+
+		Vector3[] NewVertices = new Vector3[BaseHeight.Length]; // creates a new vertices array that is the same size as the BaseHeight array
+
+		for (int Count = 0; Count < NewVertices.Length; Count++)// loops througth the entire array
+		{
+			Vector3 vertex = BaseHeight[Count]; // current vertex that will be changed
+			vertex.y += Mathf.Sin(Time.time * WaveSpeed + BaseHeight[Count].x + BaseHeight[Count].y + BaseHeight[Count].z) * Scale; // adds a sin wave
+			vertex.y += Mathf.PerlinNoise(BaseHeight[Count].x + NoiseDistance, BaseHeight[Count].y + Mathf.Sin(Time.time * 0.1f)) * NoiseStrength; // adds PerlinNoise
+			NewVertices[Count] = vertex; // adds the vertex data to the newVertices array
+		}
+
+		WaterMesh.vertices = NewVertices; // updates the meshes vertices
+		WaterMesh.RecalculateNormals(); // recalculate the mesh's normals
+	}
+}
