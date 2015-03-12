06462075d98295522e76f951f81131a7b213c5d4
Thomas-Fisher <thomas-fisher@live.co.uk>
2015-03-12 12:31:41 +0000

    Obstacle Deletion
    Add the DestroyObstacle script to all the obstacle and NPC prefabs, and
    then set the destruction distance.
    
    The script is called based upon the UpdateTime script to check if
    obstacle is behind the player, if it is, then the obstacle is deleted
    
    The rest of this task will be completed after merge due to the prefabs
    behind part of another task

---
+++ Wheel Fit/Assets/Scripts/DestroyObstacle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyObstacle : MonoBehaviour 
+{
+	public float DestructionDistance; // how far behind the player the object needs to be
+	public float UpdateTime;
+	void Start()
+	{
+		//calling the deletion check every second as calling it in update would be a waste of resourses 
+		InvokeRepeating("DestroyMe", 0, UpdateTime);
+	}
+
+	//to destroy the object
+	void DestroyMe()
+	{
+		if(transform.position.z < GameObject.Find("Player").transform.position.z - DestructionDistance) 
+		  {
+			Destroy(gameObject);
+		}
+	}
+}
