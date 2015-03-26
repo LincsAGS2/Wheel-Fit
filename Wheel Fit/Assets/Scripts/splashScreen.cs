ee1a6bf23ed5be0fc8af6c25cb22c6b10b44614c
Thomas-Fisher <thomas-fisher@live.co.uk>
2015-03-26 02:40:01 +0000

    Added Collisions
    Player can now collide with the doors, depending on the door the player
    collides with the next level is loaded
    
    Added trigger colliders to the doors
    Fixed some issues with the shop model not loading

--- Wheel Fit/Assets/Scripts/splashScreen.cs
+++ Wheel Fit/Assets/Scripts/splashScreen.cs
@@ -3,6 +3,17 @@ using System.Collections;
 
 public class splashScreen : MonoBehaviour
 {
+	void Start()
+	{
+		Invoke("StartGame",2.0f);
+	}
+	void StartGame()
+	{
+		Application.LoadLevel("game_start");
+		Debug.Log("main game loaded");
+	}
+
+	/*
 	float timeLeft = 5;
 
 	void Update()
@@ -13,6 +24,6 @@ public class splashScreen : MonoBehaviour
 			Application.LoadLevel("game_start");
 			Debug.Log("main game loaded");
 		}
-	}
+	}*/
 	
 }
