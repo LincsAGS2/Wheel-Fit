c0db1abf399e1145a906ca99fa895218a31f6a59
Thomas-Fisher <thomas-fisher@live.co.uk>
2015-04-23 09:11:56 +0100

    RespawnTimer
    Changes to the respawn timer script to make the obstacles, power ups and
    npc's spawn based upon the players movement speed

--- Wheel Fit/Assets/Scripts/RespawnTimer.cs
+++ Wheel Fit/Assets/Scripts/RespawnTimer.cs
@@ -10,27 +10,53 @@ public class RespawnTimer : MonoBehaviour {
 	[Header("NPC Variables")]
 	public float PowerUPSpawnRate;
 	public GameObject PowerUPSpawnerLocation;
-
-
+	
 	[Header("Obstacle Variables")]
 	public float ObstacleSpawnRate;
 	public GameObject ObstacleSpawnerLocation;
 
+	[Header("GameManager")]
+	public GameManager gm;
 	// Use this for initialization
 	void Start ()
 	{
-		InvokeRepeating("SpawnNPC",0,NPCSpawnRate);
-		InvokeRepeating("SpawnPowerUps",0,PowerUPSpawnRate);
-		InvokeRepeating("SpawnObstacles",0,ObstacleSpawnRate);
+		gm = FindObjectOfType<GameManager> ().GetComponent<GameManager> ();
+
+		SpawnNPCInvoker();
+		SpawnPowerUpsInvoker();
+		SpawnObstaclesInvoker();
 	}
-	
+
+	//NPC's
+	void SpawnNPCInvoker()
+	{
+		SpawnNPC();
+		Invoke("SpawnNPCInvoker", NPCSpawnRate / gm.moveSpeed);
+		//Debug.Log(NPCSpawnRate / gm.moveSpeed);
+	}
 	void SpawnNPC()
 	{
 		NPCSpawnerLocation.GetComponent<NPCSpawner>().SpawnNPCs();
 	}
+
+	//Power ups
+	void SpawnPowerUpsInvoker()
+	{
+		SpawnPowerUps();
+		Invoke("SpawnPowerUpsInvoker",PowerUPSpawnRate / gm.moveSpeed);
+		//Debug.Log("Current Power Up Spawn Rate " + PowerUPSpawnRate / gm.moveSpeed);	
+	}
 	void SpawnPowerUps()
 	{
 		PowerUPSpawnerLocation.GetComponent<PowerUpSpawner> ().SpawnPowerUps ();
+	}
+
+	//Obstacles
+	void SpawnObstaclesInvoker()
+	{
+		SpawnObstacles();
+		Invoke("SpawnObstaclesInvoker",ObstacleSpawnRate / gm.moveSpeed);
+		//Debug.Log("Current Obstacle Spawn Rate " + ObstacleSpawnRate / gm.moveSpeed);
 	}
 	void SpawnObstacles()
 	{
